a14beac2c93ef36aefd993db90379311cdb9dc88
serkanboyraci <serkanboyraci@gmail.com>
2023-03-19 18:01:38 +0300

    extensions added to tableView

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -15,9 +15,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        
     }
 
 
 }
+
+extension ViewController : UITableViewDelegate {
+    
+}
+
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    }
+    
+    
+    
+}
+    
 
