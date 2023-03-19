2e8b5fc96cfcbaab3bac09e346dcaaa8e6811b2d
serkanboyraci <serkanboyraci@gmail.com>
2023-03-19 20:31:59 +0300

    false way of the uploading image

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -13,13 +13,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var data = Data()
+    var tracker = 0
+    
+    let urlStrings = ["https://images.pexels.com/photos/3774243/pexels-photo-3774243.jpeg?cs=srgb&dl=pexels-james-wheeler-3774243.jpg&fm=jpg", "https://thumbs.dreamstime.com/z/sunrise-eftang-larvik-norway-over-rocky-beach-against-blue-cloudy-skies-135578681.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
-        
         tableView.dataSource = self
         
+        data = try! Data(contentsOf: URL(string: urlStrings[tracker])!)
+        
+        imageView.image = UIImage(data: data)
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeImage))
+        
+        
+    }
+    
+    @objc func changeImage() {
+        
+        if tracker == 0 {
+            tracker += 1
+        } else {
+                tracker -= 1
+        }
+    
+        
+        data = try! Data(contentsOf: URL(string: urlStrings[1])!)
+        imageView.image = UIImage(data: data)
         
     }
 
