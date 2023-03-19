e7ce1b6074bf7821580fea3163b037924b7ee077
serkanboyraci <serkanboyraci@gmail.com>
2023-03-19 17:55:19 +0300

    created

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
